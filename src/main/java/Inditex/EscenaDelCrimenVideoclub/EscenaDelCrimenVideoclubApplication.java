356e1bc7ba91cbf96421216174fcfb4953bd150e
webermuros1 <webermuros1@gmail.com>
2026-03-23 13:12:48 +0100

    Initial commit

---
+++ src/main/java/Inditex/EscenaDelCrimenVideoclub/EscenaDelCrimenVideoclubApplication.java
@@ -0,0 +1,13 @@
+package Inditex.EscenaDelCrimenVideoclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscenaDelCrimenVideoclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscenaDelCrimenVideoclubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Inditex/EscenaDelCrimenVideoclub/EscenaDelCrimenVideoclubApplicationTests.java
@@ -0,0 +1,13 @@
+package Inditex.EscenaDelCrimenVideoclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscenaDelCrimenVideoclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
